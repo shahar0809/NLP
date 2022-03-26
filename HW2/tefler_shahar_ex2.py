44e572909af04b3a4d4ef4083ad92872ff7f9d58
shahar0809 <shahartefler@gmail.com>
2022-03-26 22:23:40 +0300

    Initial commit - HW2

---
+++ HW2/tefler_shahar_ex2.py
@@ -0,0 +1,46 @@
+from sys import argv
+
+
+# Your implemented classes from Ex1, you may change them here according to your needs:
+
+
+class Token:
+
+    def __init__(self):
+        return
+
+
+class Sentence:
+
+    def __init__(self):
+        return
+
+
+class Corpus:
+
+    def __init__(self):
+        return
+
+
+# Implement an n-gram language model class, that will be built using a corpus of type "Corpus" (thus, you will need to
+# connect it in any way you want to the "Corpus" class):
+
+
+class NGramModel:
+
+    def __init__(self):
+        return
+
+
+if __name__ == '__main__':
+
+    xml_dir = argv[1]          # directory containing xml files from the BNC corpus, full path
+    output_file = argv[2]      # output file name, full path
+
+    # Implement here your program:
+    # 1. Create a corpus from the file in the given directory.
+    # 2. Create a language model based on the corpus.
+    # 3. Calculate and print onto the output file the first task, in the wanted format.
+    # 4. Print onto the output file the results from the second task in the wanted format.
+
+
